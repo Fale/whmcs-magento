a14ff7aceafc6471aa8e33c6cdbe9b49d6bd92ae
Fabio Alessandro Locati <flocati@grimp.eu>
2012-02-25 19:28:46 +0100

    add db settings

---
+++ settings/db.php
@@ -0,0 +1,16 @@
+<?php
+
+### MAGENTO DEMO ###
+$mysqlHost = "localhost";
+$mysqlUser = "d";
+$mysqlPass = "c00f9dbd";
+$mysqlDb = "demo_main";
+
+
+### DEVEL ###
+#$mysqlHost = "localhost";
+#$mysqlUser = "root";
+#$mysqlPass = "c00f9dbd";
+#$mysqlDb = "wiki-twitter";
+
+?>
