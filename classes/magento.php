5c2737a56e5437b05c99d48842eccd37fdf989fa
Fabio Alessandro Locati <flocati@grimp.eu>
2012-02-25 19:40:35 +0100

    add class magento and a test file

---
+++ classes/magento.php
@@ -0,0 +1,27 @@
+<?php
+require_once( "../settings/db.php" );
+require_once( "db.php" );
+
+class Magento
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Db();
+    }
+
+    public function getRevenue( $store, $from = "2000-01-01", $to = "2100-01-01", $tax = true )
+    {
+        if( $tax = true )
+            $c = "row_total_incl_tax";
+        else
+            $c = "row_total";
+        $revs = $this->db->query( "SELECT $c FROM sales_flat_oder_item WHERE `created_at` BETWEEN #$from# AND #$to#";
+        $r = 0;
+        foreach( $revs as $rev )
+            $r = $r + $rev['sales_flat_order_item'];
+        return $r;
+    }
+}
+?>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+require_once( "classes/magento.php" );
+
+$m = new Magento();
+$m->revenue( "1" );
